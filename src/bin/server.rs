2e1edc2c3e8d3055a5fa13f5f4c3a2483fb43054
gong023 <gon.gong.gone@gmail.com>
2019-09-26 15:39:25 -0700

    add license

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,4 +1,19 @@
-// https://github.com/pingcap/grpc-rs/blob/86c2bc1aec0757063de427b0ace79226489ffd78/tests-and-examples/examples/hello_world/server.rs
+// Copyright 2019 gong023
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// copied from https://github.com/pingcap/grpc-rs/blob/86c2bc1aec0757063de427b0ace79226489ffd78/tests-and-examples/examples/hello_world/server.rs
+// changed to comply with compiling.
+
 #[macro_use]
 extern crate log;
 
--- src/log_utils.rs
+++ src/log_utils.rs
@@ -1,3 +1,18 @@
+// Copyright 2019 gong023
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// copied from https://github.com/pingcap/grpc-rs/blob/86c2bc1aec0757063de427b0ace79226489ffd78/tests-and-examples/examples/log_util.rs.
+
 extern crate slog;
 extern crate slog_async;
 extern crate slog_scope;
